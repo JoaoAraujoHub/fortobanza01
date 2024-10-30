5021a571e02c32c2e4b927bb91d93672703c6df6
JoaoAraujo <araujo.joao1@aluno.ifsp.edu.br>
2024-10-30 12:58:23 -0300

    opaaa

--- fortobanza1/Model/propertyDAO.cs
+++ fortobanza1/Model/propertyDAO.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
+using System.Drawing;
 using System.Linq;
 using System.Net.Configuration;
 using System.Runtime.InteropServices;
@@ -19,6 +20,9 @@ namespace fortobanza1.Model
 
         private Connection Connect { get; set; }
         private SqlCommand Command { get; set; }
+        public object Cmd { get; private set; }
+        public object Con { get; private set; }
+
         //Sempre o nome da classe
         public propertyDAO()
         {
@@ -43,7 +47,7 @@ namespace fortobanza1.Model
             catch (Exception err)
             {
                 throw new Exception("Erro: Problemas ao inserir " +
-                    "imóvel no banco.\n" + err.Message);
+                    "dados no banco.\n" + err.Message);
             }
             finally
             {
@@ -51,9 +55,76 @@ namespace fortobanza1.Model
             }
         }
 
-        internal void Insert(Property property)
+        public void Update(Property prop)
         {
-            throw new NotImplementedException();
+            Command.Connection = Connect.ReturnConnection();
+            Command.CommandText = @"UPDATE Property SET 
+            Objective = @Objective,
+            Email = @Email,
+            Senha = @Senha
+            WHERE Id = @Id";
+            try
+            {
+                Cmd.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro: Problemas ao realizar atualização de usuário no banco.\n" + err.Message);
+            }
+            finally
+            {
+                Con.CloseConnection();
+            }
+        }
+
+        public void Excluir(int idUsuario)
+        {
+            Cmd.Connection = Con.ReturnConnection();
+            Cmd.CommandText = @"DELETE FROM Usuarios WHERE Id = @id";
+            Cmd.Parameters.AddWithValue("@id", idUsuario);
+            try
+            {
+                Cmd.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro: Problemas ao excluir usuário no banco.\n" + err.Message);
+            }
+            finally
+            {
+                Con.CloseConnection();
+            }
+        }
+        public List<Usuario> ListarTodosUsuarios()
+        {
+
+            Cmd.Connection = Con.ReturnConnection();
+            Cmd.CommandText = "SELECT * FROM Usuarios";
+
+            List<Usuario> listaDeUsuarios = new List<Usuario>(); //Instancio a list com o tamanho padrão.
+            try
+            {
+                SqlDataReader rd = Cmd.ExecuteReader();
+
+                //Enquanto for possível continuar a leitura das linhas que foram retornadas na consulta, execute.
+                while (rd.Read())
+                {
+                    Usuario usuario = new Usuario((int)rd["Id"],
+                        (string)rd["Nome"], (string)rd["Email"], (string)rd["Cpf"],
+                        (string)rd["Telefone"], (string)rd["Usuario"],
+                        "000");
+                    listaDeUsuarios.Add(usuario);
+                }
+      
+            catch (Exception err)
+            {
+                throw new Exception("Erro: Problemas ao realizar leitura de usuários no banco.\n" + err.Message);
+            }
+            finally
+            {
+                Con.CloseConnection();
+            }
+
+            return listaDeUsuarios;
         }
     }
-}
